d7a08fea6890e9fcd82a13b2762e3e01593f1527
CuadrosA <20100754@ue.edu.pe>
2023-04-24 23:18:38 -0500

    Final Update

--- main.js
+++ main.js
@@ -34,14 +34,16 @@ const Atletico_grau = "<h1>Atletico Grau</h1>" +
     "<h2>Posición actual en la tabla: 13</h2>\n" +
     "<h3>Fecha de fundación: 5 de junio de 1919</h3>\n" +
     "<br>" +
-    "<h2>Historia:</h2>";
+    "<h2>Historia:</h2>" +
+    "<p>El Atlético Grau fue fundado un 5 de junio de 1919 bajo el nombre de Club Miguel Grau, según consta en actas y a pedido de quien se considera fundador, Guillermo Herrera. El hecho tuvo lugar en el domicilio de don Juan Seminario Vinces, en la calle Tacna de Piura, frente a la casona donde naciera don Miguel Grau Seminario, a las 9 de la noche. Don José Erquiaga fue su primer presidente. El club es uno de los fundadores de la Liga Provincial de Fútbol de Piura el 22 de febrero de 1922, participando desde entonces en los campeonatos de selección y competencia, como antaño se denominaba a los torneos oficiales.</p>";
 document.getElementById("Atletico_grau").innerHTML = Atletico_grau;
 
 const Carlos_Mannucci = "<h1>Carlos A. Mannucci</h1>" +
     "<h2>Posición actual en la tabla: 04</h2>\n" +
     "<h3>Fecha de fundación: 18 de junio de 1929</h3>\n" +
     "<br>" +
-    "<h2>Historia:</h2>";
+    "<h2>Historia:</h2>" +
+    "<p>Fundado el 16 de noviembre de 1959, la institución nace de la iniciativa de las jugadoras de voleibol del Hospital Víctor Lazarte, que cuando perdieron el apoyo de su institución recurrieron a los descendientes de Carlos Alberto Mannucci Finochetti, fallecido tres años antes, para solicitarles su patrocinio y continuar participando en los torneos locales. Aceptando esto, Carlos José Mannucci Vega y su madre, Laura Vega de Mannucci es que se inicia la historia del cuadro tricolor.\nInicialmente el club empieza a competir primero en los torneos locales de voleibol y luego de básquetbol, logrando grandes actuaciones en cada torneo en el que participó. El Club en ese momento era financiado por la firma Carlos A. Mannucci S.A. y tras comprar la categoría a la escuadra del Club Mariscal Ramón Castilla inicia su participación en el fútbol trujillano. El entusiasmo de la familia Mannucci así como de los trabajadores de la empresa fomenta un interés cada vez mayor en llevar muy lejos al equipo de fútbol.</p>";
 document.getElementById("Carlos_Mannucci").innerHTML = Carlos_Mannucci;
 
 const Cienciano = "<h1>Cienciano</h1>" +
@@ -49,7 +51,7 @@ const Cienciano = "<h1>Cienciano</h1>" +
     "<h3>Fecha de fundación: 8 de julio de 1901</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>El Club Cienciano, más conocido simplemente como Cienciano, es un club de la ciudad del Cusco. Es el único club peruano campeón a nivel internacional, por conseguir dos trofeos de Conmebol: la Copa Sudamericana y Recopa Sudamericana respectivamente .​ Fue fundado en la ciudad del Cusco el 8 de julio de 1901, es un club representativo de la ciudad imperial junto a su clásico rival El Deportivo Garcilaso, siendo así qué dividen la población cusqueña entre sus simpatizantes.</p>";
 document.getElementById("Cienciano").innerHTML = Cienciano;
 
 const Cusco_FC = "<h1>Cusco FC</h1>" +
@@ -65,7 +67,7 @@ const Deportivo_binacional = "<h1>Deportivo Binacional</h1>" +
     "<h3>Fecha de fundación: 18 de diciembre de 2010</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>En el año 2010 por la gran afluencia de pobladores aficionadas al fútbol de la ciudad fronteriza de Desaguadero, el entonces alcalde de dicha localidad Juan Carlos Aquino, decide la fundación de un equipo de fútbol que pueda representar a su ciudad y ganar notoriedad en canchas puneñas, es así que se funda el club bajo el nombre de Club Deportivo Binacional de Desaguadero, llevando el nombre Binacional debido a que la ciudad donde fue fundada es una zona fronteriza entre Perú y Bolivia, ese mismo año no pudo participar en la Copa Perú ya que la misma se encontraba en fases finales.</p>";
 document.getElementById("Deportivo_binacional").innerHTML = Deportivo_binacional;
 
 const Deportivo_Municipal = "<h1>Deportivo Municipal</h1>" +
@@ -73,15 +75,15 @@ const Deportivo_Municipal = "<h1>Deportivo Municipal</h1>" +
     "<h3>Fecha de fundación: 27 de julio de 1935</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>Deportivo Municipal tiene más de una historia que enriqueció su vida institucional y que ha dado más de una alegría a todos sus hinchas. Esta pasión se formó en la década de 1930, cuando el fútbol ya comenzaba a convertirse en una actividad llamativa en la sociedad limeña.A mitad de 1934, los Sres. Ángel Pisani Escurra (jefe de limpieza pública), Dn. Ricardo Ghersi (jefe de mecánicos y talleres), conjuntamente con Dn. Arturo Martínez (jefe de la sección padrones de la Municipalidad de Lima), comenzaron a trabajar una idea de lo que sería la formación de un club de fútbol, naciendo de esta manera «Círculo Deportivo Municipal».</p>";
 document.getElementById("Deportivo_Municipal").innerHTML = Deportivo_Municipal;
 
 const Football_Club_Melgar = "<h1>Football Club Melgar</h1>" +
     "<h2>Posición actual en la tabla: 17</h2>\n" +
-    "<h3>Fecha de fundación: </h3>\n" +
+    "<h3>Fecha de fundación: 25 de marzo de 1915</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>Foot Ball Club Melgar, conocido como FBC Melgar, o simplemente Melgar, es una institución deportiva con sede en la ciudad de Arequipa, Perú. Fue fundado el 25 de marzo de 1915 por un grupo de jóvenes que deseaban practicar este deporte. Es el club más tradicional y representativo de Arequipa y cuenta con un gran número de aficionados en la región sur del país.</p>";
 document.getElementById("Football_Club_Melgar").innerHTML = Football_Club_Melgar;
 
 const Garcliaso_Del_Cusco = "<h1>DEPORTIVO GARCILASO</h1>" +
@@ -89,15 +91,16 @@ const Garcliaso_Del_Cusco = "<h1>DEPORTIVO GARCILASO</h1>" +
     "<h3>Fecha de fundación: 13 de abril de 1957</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>Fue fundado en el año 1957 bajo el nombre de Club Deportivo Lucre por los hermanos Garmendia quienes a su vez vendieron el club a monseñor Publio Prada Torres quien era padre en la iglesia de San Blas quien impulsó la práctica del fútbol entre los alumnos de la Gran Unidad Escolar Inca Garcilaso de la Vega. Desde su aparición ha mantenido una fuerte rivalidad con el club Cienciano, formado por alumnos del Colegio Nacional de Ciencias. Esta rivalidad entre ambas instituciones educativas trascendió al fútbol dando origen al Clásico Cusqueño.En 1970 fue invitado por la Asociación Peruana de Fútbol a participar en la Copa Presidente de la República. Tras superar la fase de grupos fue eliminado por Porvenir Miraflores.</p>";
 document.getElementById("Garcliaso_Del_Cusco").innerHTML = Garcliaso_Del_Cusco;
 
-const Sport_Boys = "<h1>ADT</h1>" +
+const Sport_Boys = "<h1>Sport Boys</h1>" +
     "<h2>Posición actual en la tabla: 16</h2>\n" +
-    "<h3>Fecha de fundación: </h3>\n" +
+    "<h3>Fecha de fundación: 28 de julio de 1927</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>El Sport Boys fue fundado el 28 de julio de 1927 por un grupo de entusiastas alumnos del Colegio Maristas del Callao, cuyas edades fluctuaban entre los 11 y 15 años, se reunieron, desde tempranas horas de la noche del 27, en la casa de Ricardo Arbe, situada en la Avenida Sáenz Peña signada con el número 724, Solar Santa Ana. A las 0:00 horas del 28, el grupo entonó el Himno Nacional recordando el 106 aniversario de la Independencia Nacional, y de inmediato empezó la deliberación para formar la primera junta directiva. La presidencia recayó en el joven que lanzó la idea: Gualberto Lizárraga, el Secretario y primer arquero del equipo fue Nicolas Mandriotti." + "<br>" +
+    "El nombre del club, decidido por unanimidad, fue Sport Boys Association que fue propuesto por el mismo Gualberto Lizárraga derivado de un club de natación de la época llamado 'Old Boys' donde practicaba ese deporte. El original color de su camiseta les hizo ganarse el apelativo de 'Los Rosados' aunque también se ganaron el apelativo de Porteños por nacer en el primer puerto del Perú. Muchos años después, esta relación entre el puerto, el mar, el fútbol y los goles les darían otro sobrenombre más: 'La Misilera'.</p>";
 document.getElementById("Sport_Boys").innerHTML = Sport_Boys;
 
 const Sport_Huancayo = "<h1>Sport Huancayo</h1>" +
@@ -110,7 +113,7 @@ document.getElementById("Sport_Huancayo").innerHTML = Sport_Huancayo;
 
 const Sporting_Cristal = "<h1>Sporting Cristal</h1>" +
     "<h2>Posición actual en la tabla: 05</h2>\n" +
-    "<h3>Fecha de fundación: </h3>\n" +
+    "<h3>Fecha de fundación: 13 de diciembre de 1955</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
     "<p>El Club Sporting Cristal fue fundado el 13 de diciembre de 1955 en el distrito del Rímac por el ingeniero Ricardo Bentín Mujica y su esposa Esther Grande de Bentín. Durante toda su historia, el equipo celeste logró 20 títulos nacionales. Además, fue el último equipo peruano que clasificó a la final de la Copa Libertadores en 1997.</p>";
@@ -124,12 +127,12 @@ const UCV = "<h1>UNIVERSIDAD CÉSAR VALLEJO</h1>" +
     "<p>El Club Deportivo Universidad César Vallejo, más conocido por las siglas UCV, fue fundada el 06 de enero de 1996 por César Acuña Peralta. Este equipo representa a Trujillo, ciudad donde participó desde torneos distritales hasta llegar a la Primera División del Perú en el año 2004. El cuadro poeta ha sido campeón del Torneo del Inca (2015), Segunda División (2007 y 2018) y Copa Perú (2001).</p>";
 document.getElementById("UCV").innerHTML = UCV;
 
-const Union_Comercio = "<h1>Union Comercio</h1>" +
+const Union_Comercio = "<h1>Club Deportivo Unión Comercio</h1>" +
     "<h2>Posición actual en la tabla: 15</h2>\n" +
-    "<h3>Fecha de fundación: </h3>\n" +
+    "<h3>Fecha de fundación: 31 de enero de 2002</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>El Club Deportivo Unión Comercio fue fundado el 31 de enero de 2002 en el distrito de Nueva Cajamarca. La creación se dio por comerciantes del Mercado Central de Nueva Cajamarca, quienes organizaban torneos locales amistosos en su ciudad entre diversos cuadros aficionados, siendo allí de donde surgió este club.</p>";
 document.getElementById("Union_Comercio").innerHTML = Union_Comercio;
 
 const Universitario = "<h1>Universitario</h1>" +
@@ -142,8 +145,8 @@ document.getElementById("Universitario").innerHTML = Universitario;
 
 const UTC_Cajamarca = "<h1>UTC Cajamarca</h1>" +
     "<h2>Posición actual en la tabla: 10</h2>\n" +
-    "<h3>Fecha de fundación: </h3>\n" +
+    "<h3>Fecha de fundación: 14 de julio de 1964</h3>\n" +
     "<br>" +
     "<h2>Historia:</h2>" +
-    "<p></p>";
+    "<p>En el año 1964, Universidad Técnica de Cajamarca (actualmente Universidad Nacional de Cajamarca) inicia su vida Institucional con ella surgen ideas renovadoras y reivindicatorias para Cajamarca, en el aspecto social, cultural y deportivo. En este último el panorama era negativo especialmente en la disciplina de fútbol, los representativos cajamarquinos eran humillados con sendas derrotas por elencos de ciudades más pequeñas como San Pedro y Guadalupe. Ante esta triste realidad y con el ansia de reivindicar al fútbol cajamarquino, surge la idea de formar un Club Deportivo. Así trabajadores administrativos, docentes y alumnos contagiados con la idea y el entusiasmo, deciden poner en marcha la brillante idea de organizar un club deportivo que milite en la Liga Distrital de Fútbol.</p>";
 document.getElementById("UTC_Cajamarca").innerHTML = UTC_Cajamarca;
