5e1f8c2f29afa94bd7f95269cc58029fd2478ea8
CuadrosA <20100754@ue.edu.pe>
2023-04-24 22:33:37 -0500

    Update v3

---
+++ main.js
@@ -0,0 +1,149 @@
+const cantolao = "<h1>CANTOLAO</h1>" +
+    "<h2>Posición actual en la tabla: 19</h2>\n" +
+    "<h3>Fecha de fundación: 14 de abril de 1981</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>La Academia Deportiva Cantolao es una institución peruana dedicada a la difusión de fútbol en sus distintas categorías. El cuadro «Delfín» fue fundado en el 14 de abril de 1981 y participa en la Primera División del Perú desde el año 2017.</p>";
+document.getElementById("cantolao").innerHTML = cantolao;
+
+const ADT_Tarma = "<h1>ADT</h1>" +
+    "<h2>Posición actual en la tabla: 08</h2>\n" +
+    "<h3>Fecha de fundación: 18 de junio de 1929</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>La Asociación Deportiva Tarma es un club peruano oriundo de la ciudad de Tarma en el departamento de Junín. El «Vendaval de Tarma» fue fundado el 18 de junio de 1929 y en el 2022 consiguió su retornó a la Primera División del Perú después de 31 años.</p>";
+document.getElementById("ADT_Tarma").innerHTML = ADT_Tarma;
+
+const Alianza_Atletico = "<h1>Alianza Atletico</h1>" +
+    "<h2>Posición actual en la tabla: 09</h2>\n" +
+    "<h3>Fecha de fundación: 18 de enero de 1920</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>El Club Alianza Atlético de Sullana es un equipo de fútbol de la ciudad de Sullana en el departamento de Piura. El «Vendaval del Norte» fue fundado el 18 de enero de 1920 y es uno de los cuadros más populares del norte del país. El elenco de Sullana regresó a la Primera División del Perú en el año de su centenario.</p>";
+document.getElementById("Alianza_Atletico").innerHTML = Alianza_Atletico;
+
+const Alianza_Lima = "<h1>Alianza Lima</h1>" +
+    "<h2>Posición actual en la tabla: 01</h2>\n" +
+    "<h3>Fecha de fundación: 15 de febrero de 1901</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>El Club Alianza Lima es una institución deportiva y social nacida en la ciudad de Lima. El cuadro «blanquiazul» fue fundado el 15 de febrero de 1901 y actualmente es el club más longevo que disputa la Primera División del Perú, además de ser el vigente bicampeón del balompié nacional.</p>";
+document.getElementById("Alianza_Lima").innerHTML = Alianza_Lima;
+
+const Atletico_grau = "<h1>Atletico Grau</h1>" +
+    "<h2>Posición actual en la tabla: 13</h2>\n" +
+    "<h3>Fecha de fundación: 5 de junio de 1919</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>";
+document.getElementById("Atletico_grau").innerHTML = Atletico_grau;
+
+const Carlos_Mannucci = "<h1>Carlos A. Mannucci</h1>" +
+    "<h2>Posición actual en la tabla: 04</h2>\n" +
+    "<h3>Fecha de fundación: 18 de junio de 1929</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>";
+document.getElementById("Carlos_Mannucci").innerHTML = Carlos_Mannucci;
+
+const Cienciano = "<h1>Cienciano</h1>" +
+    "<h2>Posición actual en la tabla: 14</h2>\n" +
+    "<h3>Fecha de fundación: 8 de julio de 1901</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Cienciano").innerHTML = Cienciano;
+
+const Cusco_FC = "<h1>Cusco FC</h1>" +
+    "<h2>Posición actual en la tabla: 02</h2>\n" +
+    "<h3>Fecha de fundación: 16 de julio del 2009</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>Cusco Fútbol Club fue fundado el 16 de julio de 2009 bajo el nombre de Asociación Civil Real Atlético Garcilaso. Su crecimiento fue tan rápido que en el 2011 ganó la Copa Perú y logró su ascenso a la Primera División. Fue dos veces subcampeón nacional (2012 y 2013) y logró una gran campaña en la Copa Libertadores del 2013 llegando a cuartos de final. A fines del 2019, la directiva del conjunto cusqueño decidió cambiarle el nombre al equipo.</p>";
+document.getElementById("Cusco_FC").innerHTML = Cusco_FC;
+
+const Deportivo_binacional = "<h1>Deportivo Binacional</h1>" +
+    "<h2>Posición actual en la tabla: 18</h2>\n" +
+    "<h3>Fecha de fundación: 18 de diciembre de 2010</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Deportivo_binacional").innerHTML = Deportivo_binacional;
+
+const Deportivo_Municipal = "<h1>Deportivo Municipal</h1>" +
+    "<h2>Posición actual en la tabla: 12</h2>\n" +
+    "<h3>Fecha de fundación: 27 de julio de 1935</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Deportivo_Municipal").innerHTML = Deportivo_Municipal;
+
+const Football_Club_Melgar = "<h1>Football Club Melgar</h1>" +
+    "<h2>Posición actual en la tabla: 17</h2>\n" +
+    "<h3>Fecha de fundación: </h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Football_Club_Melgar").innerHTML = Football_Club_Melgar;
+
+const Garcliaso_Del_Cusco = "<h1>DEPORTIVO GARCILASO</h1>" +
+    "<h2>Posición actual en la tabla: 07</h2>\n" +
+    "<h3>Fecha de fundación: 13 de abril de 1957</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Garcliaso_Del_Cusco").innerHTML = Garcliaso_Del_Cusco;
+
+const Sport_Boys = "<h1>ADT</h1>" +
+    "<h2>Posición actual en la tabla: 16</h2>\n" +
+    "<h3>Fecha de fundación: </h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Sport_Boys").innerHTML = Sport_Boys;
+
+const Sport_Huancayo = "<h1>Sport Huancayo</h1>" +
+    "<h2>Posición actual en la tabla: 11</h2>\n" +
+    "<h3>Fecha de fundación: 07 de febrero de 2007</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>El Club Sport Huancayo, conocido como el ‘Rojo Matador’, se fundó el 07 de febrero del 2007 y lleva este nombre por el cariño y la identificación con la ciudad. Al año siguiente, ganó la Copa Perú y desde el 2009 viene participando en Primera División del Perú.</p>";
+document.getElementById("Sport_Huancayo").innerHTML = Sport_Huancayo;
+
+const Sporting_Cristal = "<h1>Sporting Cristal</h1>" +
+    "<h2>Posición actual en la tabla: 05</h2>\n" +
+    "<h3>Fecha de fundación: </h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>El Club Sporting Cristal fue fundado el 13 de diciembre de 1955 en el distrito del Rímac por el ingeniero Ricardo Bentín Mujica y su esposa Esther Grande de Bentín. Durante toda su historia, el equipo celeste logró 20 títulos nacionales. Además, fue el último equipo peruano que clasificó a la final de la Copa Libertadores en 1997.</p>";
+document.getElementById("Sporting_Cristal").innerHTML = Sporting_Cristal;
+
+const UCV = "<h1>UNIVERSIDAD CÉSAR VALLEJO</h1>" +
+    "<h2>Posición actual en la tabla: 06</h2>\n" +
+    "<h3>Fecha de fundación: 06 de enero de 1996</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>El Club Deportivo Universidad César Vallejo, más conocido por las siglas UCV, fue fundada el 06 de enero de 1996 por César Acuña Peralta. Este equipo representa a Trujillo, ciudad donde participó desde torneos distritales hasta llegar a la Primera División del Perú en el año 2004. El cuadro poeta ha sido campeón del Torneo del Inca (2015), Segunda División (2007 y 2018) y Copa Perú (2001).</p>";
+document.getElementById("UCV").innerHTML = UCV;
+
+const Union_Comercio = "<h1>Union Comercio</h1>" +
+    "<h2>Posición actual en la tabla: 15</h2>\n" +
+    "<h3>Fecha de fundación: </h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("Union_Comercio").innerHTML = Union_Comercio;
+
+const Universitario = "<h1>Universitario</h1>" +
+    "<h2>Posición actual en la tabla: 03</h2>\n" +
+    "<h3>Fecha de fundación: 07 de agosto de 1924</h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p>El Club Universitario de Deportes fue fundado el 7 de agosto de 1924. Un grupo de estudiantes de la Universidad Nacional Mayor de San Marcos dieron inicio a esta historia creando la «Federación Universitaria de Fútbol» y que años más tarde llevaría el nombre con el actualmente compite en nuestro torneo. Al conjunto estudiantil se le conoce popularmente como Universitario, la U o los ‘Cremas’, y es en el equipo más antiguo que ha permanecido de manera ininterrumpida en la máxima categoría del fútbol profesional peruano. Torneo del que viene participando desde 1928.</p>";
+document.getElementById("Universitario").innerHTML = Universitario;
+
+const UTC_Cajamarca = "<h1>UTC Cajamarca</h1>" +
+    "<h2>Posición actual en la tabla: 10</h2>\n" +
+    "<h3>Fecha de fundación: </h3>\n" +
+    "<br>" +
+    "<h2>Historia:</h2>" +
+    "<p></p>";
+document.getElementById("UTC_Cajamarca").innerHTML = UTC_Cajamarca;
